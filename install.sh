063140316be1056c9749606955ec29173588d280
Adam Szlachta <adam.szlachta@gmail.com>
2015-08-25 12:09:29 +0200

    Install script improvments

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 rm -f ~/.gitconfig ~/.gitignore_global ~/.tmux.conf ~/.zshrc
+
 ln -s ~/.dot/.gitconfig ~/.gitconfig
 ln -s ~/.dot/.gitignore_global ~/.gitignore_global
 ln -s ~/.dot/.tmux.conf ~/.tmux.conf
+
 mkdir -p ~/.hammerspoon
 rm -f ~/.hammerspoon/init.lua
 ln -s ~/.dot/.hammerspoon/init.lua ~/.hammerspoon/init.lua
-touch ~/.zshrc.local
+
+if [ -f ~/.zshrc.local ];
+then
+    touch ~/.zshrc.local
+else
+    echo "source ~/.dot/.zshrc" > ~/.zshrc.local
+fi
