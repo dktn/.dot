c106e5e92d20b94689f1da140343c69e52d38543
Adam Szlachta <adam.szlachta@gmail.com>
2015-12-22 01:44:03 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-rm -f ~/.gitconfig ~/.gitignore_global ~/.tmux.conf ~/.zshrc
+rm -f ~/.tmux.conf
+ln -s ~/.dot/.tmux.conf ~/.tmux.conf
 
+rm -f ~/.zshrc
+ln -s ~/.dot/.zshrc ~/.zshrc
+
+rm -f ~/.oh-my-zsh/themes/agnoster-dktn-zsh.theme
+ln -s ~/.dot/agnoster-dktn-zsh.theme ~/.oh-my-zsh/themes/agnoster-dktn-zsh.theme
+
+rm -f ~/.gitconfig ~/.gitignore_global
 ln -s ~/.dot/.gitconfig ~/.gitconfig
 ln -s ~/.dot/.gitignore_global ~/.gitignore_global
-ln -s ~/.dot/.tmux.conf ~/.tmux.conf
-ln -s ~/.dot/.zshrc ~/.zshrc
 
 rm -rf ~/.hammerspoon
 ln -s ~/.dot/.hammerspoon ~/.hammerspoon
