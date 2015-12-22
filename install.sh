42f9f30aa6a6aeea30df0345057ed24353467d3e
Adam Szlachta <adam.szlachta@gmail.com>
2015-12-22 01:46:15 +0100

    Fixed typo

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ ln -s ~/.dot/.tmux.conf ~/.tmux.conf
 rm -f ~/.zshrc
 ln -s ~/.dot/.zshrc ~/.zshrc
 
-rm -f ~/.oh-my-zsh/themes/agnoster-dktn-zsh.theme
-ln -s ~/.dot/agnoster-dktn-zsh.theme ~/.oh-my-zsh/themes/agnoster-dktn-zsh.theme
+rm -f ~/.oh-my-zsh/themes/agnoster-dktn.zsh-theme
+ln -s ~/.dot/agnoster-dktn.zsh-theme ~/.oh-my-zsh/themes/agnoster-dktn.zsh-theme
 
 rm -f ~/.gitconfig ~/.gitignore_global
 ln -s ~/.dot/.gitconfig ~/.gitconfig
