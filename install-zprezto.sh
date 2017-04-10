0c6a70f061116c877442f0337a9f9551c3c28733
Adam Szlachta <adam.szlachta@gmail.com>
2017-04-10 03:03:05 +0200

    Add custom sorinx prompt

--- install-zprezto.sh
+++ install-zprezto.sh
@@ -2,7 +2,11 @@
 
 . "install-lib.sh"
 
+export LINK_LEN=70
+
 create_symlink ".zshrc"     ".zprezto"
 create_symlink ".zpreztorc" ".zprezto"
+create_symlink ".zprezto/modules/prompt/functions/prompt_sorinx_setup"
+
 
 echo "Done."
